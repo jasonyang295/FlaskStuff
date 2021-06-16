43ca87eec3f1aaf02a2d1dc708d67348f4d3de1d
Jason Yang <canyoncrestjy@gmail.com>
2021-06-16 16:03:50 -0700

    created database

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 app = Flask(__name__)
+#initialize database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+#create class
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False) #don't want user to create new task and leave the content empty
+    date_created = db.Column(db.DateTime, default= datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
