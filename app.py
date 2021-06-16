db0a8e7dd6b28f8492cb5cb0b20df2952ec2c07c
Jason Yang <canyoncrestjy@gmail.com>
2021-06-16 16:42:11 -0700

    small change

--- app.py
+++ app.py
@@ -3,37 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-#initialize database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-#create class
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False) #don't want user to create new task and leave the content empty
-    date_created = db.Column(db.DateTime, default= datetime.utcnow)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
 
-
-
-@app.route('/', methods= ['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST': 
+    if request.method == 'POST':
         task_content = request.form['content']
         new_task = Todo(content=task_content)
 
-        try: 
+        try:
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
-        except: 
-            return "there was an issue adding your task"
+        except:
+            return 'There was an issue adding your task'
+
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -61,6 +59,7 @@ def update(id):
 
     else:
         return render_template('update.html', task=task)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
